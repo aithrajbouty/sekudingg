511e17d826da806fe6d7e1d50a4f4cbb3e9fc797
Sharashena Chairani <shenachairani@gmail.com>
2021-05-10 01:12:43 +0700

    benerin home

--- client/src/components/Hero.vue
+++ client/src/components/Hero.vue
@@ -1,63 +1,55 @@
 <template>
   <div class="hero">
-      <!-- Desktop -->
+    <div class="container">
       <div class="d-none d-md-block">
-          <div class="row mt-4">
-            <div class="col-md-6">
-                <div class="d-flex h-100">
-                    <div class="justify-content-center align-self-left text-left">
-                        <h2>Learn to code <strong style="color: #608EB8;">for free</strong>, <br> with <strong style="color: #608EB8;">Sekuuuding!</strong></h2>
-                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                        <button class="btn btn-lg btn-warning"><strong>Start Learning Now !</strong></button>
-                    </div>
-                </div>
+        <div class="row mt-2">
+          <div class="col-md-6">
+            <div class="d-flex h-100">
+              <div class="justify-content-center align-self-left text-left">
+                <h2>Learn to code <strong style="color: #608EB8;">for free</strong>, <br> with <strong style="color: #608EB8;">Sekuuuding!</strong></h2>
+                <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <button class="btn btn-lg btn-warning"><strong>Start Learning Now !</strong></button>
+              </div>
             </div>
-      <div class="col-md-6">
-          <img src="../assets/images/pict-a.svg" width="100%"/>
-      </div>
-  </div>
-  <br><br><br><br><br><br>
-  <div class="text-center">
-    <h2 class="text-center"><strong>How it works ?</strong></h2>
-  </div>
-  <div class="row">
-    <div id="about" class="p-3 p-md-5">
-      <div class="row">
-          <p>This is the website we wish we had when we were learning on our own. We scour the internet looking for only the best resources to supplement your learning and present them in a logical order.</p>
-        <div class="col-lg">
-          <!-- Icon made by Freepik perfect from www.flaticon.com -->
-          <img src="../assets/images/learn.png" height="120" class="m-4">
-          <h5 class="ml-md-4 mr-md-4"><strong>Learn</strong></h5>
-          <p>Learn from a curriculum with the best curated online tutorials, blogs, and courses.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="../assets/images/homebaruu.svg" width="100%"/>
+          </div>
         </div>
-        <div class="col-lg">
-          <!-- Icon made by Freepik perfect from www.flaticon.com -->
-          <img src="../assets/images/build.png" height="120" class="m-4">
-          <h5 class="ml-md-4 mr-md-4"><strong>Build</strong></h5>
-          <p>Build a portfolio-worthy projects along the way, from simple scripts to full programs and deployed websites.</p>
-        </div>
-        <div class="col-lg">
-          <!-- Icon made by Freepik perfect from www.flaticon.com -->
-          <img src="../assets/images/connect.png" height="120" class="m-4">
-          <h5 class="ml-md-4 mr-md-4"><strong>Connect</strong></h5>
-          <p>You’re not alone. Learn and get help from our friendly community of beginner and experienced developers.</p>
-        </div>
-      </div>
-    </div>
-    </div>
-
-      <div class="row mt-4">
-        <div class="col">
-          <h2><strong>Learn everything you need to know to become a Developer</strong></h2>
-        </div>
-        <div class="col">
-          <router-link to="/module" class="btn btn-warning float-right"><b-icon-file-arrow-down></b-icon-file-arrow-down><strong> View All Our Courses</strong> </router-link>
-        </div>
-      </div>
-      
-      <div class="container-fluid">
-        <div class="header-body">
-          <div class="row">
+        <div class="row-mt-4">
+           <div class="text-center">
+            <h2 class="text-center"><strong>How it works ?</strong></h2>
+            <p>This is the website we wish we had when we were learning on our own. We scour the internet looking for only the best resources to supplement your learning and present them in a logical order.</p>
+          </div>
+          <div id="about" class="p-3 p-md-3">
+            <div class="row" style="margin-top:-50px;">
+              <div class="col-lg">
+                <!-- Icon made by Freepik perfect from www.flaticon.com -->
+                <img src="../assets/images/learn.png" height="120" class="m-4">
+                <h5 class="ml-md-4 mr-md-4"><strong>Learn</strong></h5>
+                <p>Learn from a curriculum with the best curated online tutorials, blogs, and courses.</p>
+              </div>
+              <div class="col-lg">
+                <!-- Icon made by Freepik perfect from www.flaticon.com -->
+                <img src="../assets/images/build.png" height="120" class="m-4">
+                <h5 class="ml-md-4 mr-md-4"><strong>Build</strong></h5>
+                <p>Build a portfolio-worthy projects along the way, from simple scripts to full programs and deployed websites.</p>
+              </div>
+              <div class="col-lg">
+                <!-- Icon made by Freepik perfect from www.flaticon.com -->
+                <img src="../assets/images/connect.png" height="120" class="m-4">
+                <h5 class="ml-md-4 mr-md-4"><strong>Connect</strong></h5>
+                <p>You’re not alone. Learn and get help from our friendly community of beginner and experienced developers.</p>
+              </div>
+            </div>
+          </div>
+          <div class="ourmodul" style="margin-top:50px;">
+            <div class="text-center">
+            <h2><strong>Learn everything you need to know</strong></h2>
+            <h2><strong>to become a Developer</strong></h2>
+            </div>
+            <div class="container-fluid">
+               <div class="row">
             <div class="col-md-6 col-xl-4">
               <div class="card card-stats mb-4" show-footer-line="true">
                 <!----><!----><!---->
@@ -138,33 +130,93 @@
                                                                       </div>
                                                                       </div>
                                                                       </div>
-                                                                      </div>
-                                                                      </div>
 
-    <div class="mt-4">
-    <h2><strong>Our Testimoni</strong></h2>
-    <b-card img-src="https://img.icons8.com/bubbles/100/000000/check-female.png" img-alt="Card image" img-left class="mb-3">
-      <b-card-text>
-        Before I started sekuuuding I had literally ZERO 
+              
+            </div>
+              <router-link to="/module" class="btn btn-warning" style="width: 366px;height: 50px;font-size:22px;">
+              <b-icon-file-arrow-down></b-icon-file-arrow-down>
+              <strong> View All Our Courses</strong> </router-link>
+          </div>
+          <div class="ourtesti">
+            <div class="text-center">
+            <h2><strong>Our Testimoni</strong></h2>
+            </div>
+            <div class="p-3 p-md-3" style="margin-top:20px;">
+            <div class="row">
+              <div class="col-lg">
+                <div class="success-story">
+                  <div class="photo"> 
+                    <img src="../assets/images/hanapic.jpg" width="40%" class="rounded-circle" style="display: block;
+    margin-left: auto;
+    margin-right: auto;"> </div>
+
+                  <div class="success-content">
+                    <div class="details text-center">
+                      <p>Rob Pando</p>
+                    </div>
+                    <p>Before I started sekuuding I had literally ZERO 
 programming experience. After almost exactly a year of 
-working through the program, I was offered a job as a front...
-      </b-card-text>
-    </b-card>
-    <b-card img-src="https://img.icons8.com/bubbles/100/000000/administrator-male.png" img-alt="Card image" img-left class="mb-3">
-      <b-card-text>
-        Before I started sekuuuding I had literally ZERO 
+working through the program, I was offered a job as a front...</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg">
+                  <div class="success-story">
+                  <div class="photo"> 
+                    <img src="../assets/images/aithrapic.jpg" width="40%" class="rounded-circle" style="display: block;
+    margin-left: auto;
+    margin-right: auto;"> </div>
+
+                  <div class="success-content">
+                    <div class="details text-center">
+                      <p>Olivia</p>
+                    </div>
+                    <p>Before I started sekuuding I had literally ZERO 
 programming experience. After almost exactly a year of 
-working through the program, I was offered a job as a front...
-      </b-card-text>
-    </b-card>
-    <b-card img-src="https://img.icons8.com/bubbles/100/000000/cloud-account-login-male.png" img-alt="Card image" img-left class="mb-3">
-      <b-card-text>
-        Before I started sekuuuding I had literally ZERO 
+working through the program, I was offered a job as a front...</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg">
+                <div class="success-story">
+                  <div class="photo"> 
+                    <img src="../assets/images/aithrapic.jpg" width="40%" class="rounded-circle" style="display: block;
+    margin-left: auto;
+    margin-right: auto;"> </div>
+
+                  <div class="success-content">
+                    <div class="details text-center">
+                      <p>Cody Loyd</p>
+                    </div>
+                    <p>Before I started sekuuding I had literally ZERO 
 programming experience. After almost exactly a year of 
-working through the program, I was offered a job as a front...
-      </b-card-text>
-    </b-card>
-    </div>
+working through the program, I was offered a job as a front...</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg">
+                 <div class="success-story">
+                  <div class="photo"> 
+                    <img src="../assets/images/kefilpic.jpg" width="40%" class="rounded-circle" style="display: block;
+    margin-left: auto;
+    margin-right: auto;"> </div>
+
+                  <div class="success-content">
+                    <div class="details text-center">
+                      <p>William Kennedy</p>
+                    </div>
+                    <p>Before I started sekuuding I had literally ZERO 
+programming experience. After almost exactly a year of 
+working through the program, I was offered a job as a front...</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -175,18 +227,28 @@ export default {
 </script>
 
 <style>
-.hero{
-  max-width: 1190px;
-  background: url('https://pbs.twimg.com/media/Eyl8T7hU4AAc0Ur?format=png&name=small');
-  background-repeat: no-repeat;
-  background-position: right;
-  background-size: contain;
-  margin-top: -20px;
-  height: 600px;
-  position: absolute;
+.d-flex{
+  margin-top: 130px;
 }
-.d-none{
-  margin-top: 150px;
+.row-mt-4{
+  margin-top: 100px;
 }
 
+.ourtesti{
+    margin-top:80px;
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+    text-align: justify;
+    box-sizing: border-box;
+    width: 100%;
+}
+.success-stories .success-stories-container .success-story {
+    flex-basis: 49.9%;
+    display: flex;
+    padding-bottom: 36px;
+}
+*, *::before, *::after {
+    box-sizing: border-box;
+}
 </style>
